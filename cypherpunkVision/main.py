dfb35affa4c77501a358e1208ff0e30fadc56b66
Misash <misash@outlook.com>
2023-11-26 18:43:17 -0500

    cypherpunk vision with GPU

--- cypherpunkVision/main.py
+++ cypherpunkVision/main.py
@@ -1,3 +1,38 @@
+# import tensorflow as tf
+# from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+# from tensorflow.keras.preprocessing import image
+# import numpy as np
+# import requests
+# from PIL import Image
+# from io import BytesIO
+
+
+
+# # Cargar modelo preentrenado de MobileNetV2
+# modelo = MobileNetV2(weights='imagenet')
+
+
+# def preprocesar_imagen(url):
+#     respuesta = requests.get(url)
+#     img = Image.open(BytesIO(respuesta.content))
+#     img = img.resize((224, 224))
+#     img_array = image.img_to_array(img)
+#     img_array = np.expand_dims(img_array, axis=0)
+#     img_array = preprocess_input(img_array)
+#     return img_array
+
+# def detectar_objetos(url):
+#     img = preprocesar_imagen(url)
+#     predicciones = modelo.predict(img)
+#     return decode_predictions(predicciones, top=3)[0]
+
+# # url_imagen = "https://www.hogarmania.com/archivos/201911/aves-848x477x80xX.jpg"
+# url_imagen = "https://firebasestorage.googleapis.com/v0/b/cypherpunks-14935.appspot.com/o/wolf.jpg?alt=media&token=14e5675d-ab82-4a22-b04b-aa3c755ddf6b"
+# resultados = detectar_objetos(url_imagen)
+# for resultado in resultados:
+#     print(resultado)
+
+from flask import Flask, request, jsonify
 import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
@@ -6,11 +41,10 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-
+app = Flask(__name__)
 
 # Cargar modelo preentrenado de MobileNetV2
 modelo = MobileNetV2(weights='imagenet')
-
 
 def preprocesar_imagen(url):
     respuesta = requests.get(url)
@@ -26,9 +60,16 @@ def detectar_objetos(url):
     predicciones = modelo.predict(img)
     return decode_predictions(predicciones, top=3)[0]
 
-# url_imagen = "https://www.hogarmania.com/archivos/201911/aves-848x477x80xX.jpg"
-url_imagen = "https://firebasestorage.googleapis.com/v0/b/cypherpunks-14935.appspot.com/o/wolf.jpg?alt=media&token=14e5675d-ab82-4a22-b04b-aa3c755ddf6b"
-resultados = detectar_objetos(url_imagen)
-for resultado in resultados:
-    print(resultado)
+@app.route('/detect', methods=['POST'])
+def detect():
+    content = request.json
+    url_imagen = content['url']
+    resultados = detectar_objetos(url_imagen)
+    return jsonify([{"label": res[1], "confidence": float(res[2])} for res in resultados])
 
+@app.route('/', methods=['GET'])
+def hello():
+    return "cypherpunk vision"
+
+if __name__ == '__main__':
+    app.run(debug=False)
