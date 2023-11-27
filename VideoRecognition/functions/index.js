c3b7ab8ca09e994d2ceee2d34469268a57ed6dc9
Misash <misash@outlook.com>
2023-11-27 03:44:30 -0500

    videoRecognition works on cloud

--- VideoRecognition/functions/index.js
+++ VideoRecognition/functions/index.js
@@ -1,10 +1,9 @@
 
 //firebase
 const functions = require("firebase-functions");
+const { FieldValue } = require("firebase-admin/firestore");
 const admin = require("firebase-admin");
 const vision = require('@google-cloud/vision');
-
-//framer function
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const { Storage } = require('@google-cloud/storage');
@@ -13,17 +12,19 @@ const path = require('path');
 const fs = require('fs');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-
 admin.initializeApp();
 const client = new vision.ImageAnnotatorClient();
+const storage = new Storage();
+const bucketName = "cypherpunks-14935.appspot.com"; // Reemplazar con el nombre de tu bucket
+const frameRate = 1
 
 const runtimeOpts = {
-    timeoutSeconds: 540, // Aumenta a 2 minutos, ajusta según sea necesario
-    memory: '1GB' // Ajusta según sea necesario
+    timeoutSeconds: 540, 
+    memory: '1GB'
 }
 
 exports.VideoFrameAnalysis = functions.runWith(runtimeOpts).storage.object().onFinalize(async (object) => {
-    // Verifica si el archivo es un video
+     // Verifica si el archivo es un video
     if (!object.contentType.startsWith("video/")) {
         console.log("Este archivo no es un video.");
         return null;
@@ -32,42 +33,56 @@ exports.VideoFrameAnalysis = functions.runWith(runtimeOpts).storage.object().onF
     // Construye la ruta del archivo sin el prefijo 'gs://'
     const filePath = object.name;
 
-    console.log("Nombre del archivo:", object.name);
-    console.log("Ruta del archivo:", filePath);
-
+    // console.log("Nombre del archivo:", object.name);
+    // console.log("Ruta del archivo:", filePath);
 
     // Extrae frames del video
-    // const frames = await extractFramesFromVideo(filePath);
     const { frames, tempLocalFile, frameDir } = await extractFramesFromVideo(filePath);
 
 
-    // Analiza cada frame con Vision API
+    // Subir frames a Firebase Storage y analizar con Vision API
+    const frameStoragePath = `Frames/${path.basename(filePath)}`;
     for (let i = 0; i < frames.length; i++) {
         const frame = frames[i];
+        // const frameName = path.basename(frame);
+        const frameName = `frame_${String(i).padStart(4, '0')}.png`;
+        const frameUploadPath = `${frameStoragePath}/${frameName}`;
+        await storage.bucket(bucketName).upload(frame, {destination: frameUploadPath});
+       
+
+        
         const [result] = await client.labelDetection(frame);
         const labels = result.labelAnnotations;
-        // console.log(`Etiquetas encontradas en frame ${i}`);
-        console.log(`Etiquetas encontradas en frame ${i}: ${labels.map(label => label.description)}`);
+        // console.log(`Etiquetas encontradas en frame ${i}: ${labels.map(label => label.description)}`);
 
-        // Subir las etiquetas a Firebase Firestore
+        // Subir las etiquetas y la información del frame a Firestore
         const db = admin.firestore();
-        const docRef = db.collection('Videos').doc(object.name).collection('Frames');
-        await docRef.add({
-            frameId: `frame_${i}`,
-            labels: labels.map(label => label.description)
-        });
+
+        // Actualizar el índice invertido para cada etiqueta
+        for (const label of labels) {
+            const labelDesc = label.description;
+            const labelRef = db.collection('VideosLabels').doc(labelDesc);
+            const labelDoc = await labelRef.get();
+            if (!labelDoc.exists) {
+                await labelRef.set({ frames: [frameUploadPath] });
+            } else {
+                await labelRef.update({
+                    frames: FieldValue.arrayUnion(frameUploadPath)
+                });
+            }
+        }
     }
 
-  
-    // Limpieza: elimina el directorio de frames y el archivo de video temporal
+    // clean
     await cleanUp(tempLocalFile, frameDir);
-
 
     return null;
 });
 
 
-async function extractFramesFromVideo(filePath, frameRate = 1) {
+
+
+async function extractFramesFromVideo(filePath) {
 
     const storage = new Storage();
     const uniqueSuffix = `${Date.now()}-${Math.random()}`; // Genera un sufijo único
@@ -76,24 +91,24 @@ async function extractFramesFromVideo(filePath, frameRate = 1) {
     const frameDir = path.join(tempLocalDir, 'frames');
     const bucketName = "cypherpunks-14935.appspot.com";
 
-    console.log("Archivo temporal local:", tempLocalFile);
+    // console.log("Archivo temporal local:", tempLocalFile);
     // Verifica y limpia si es necesario
     if (fs.existsSync(tempLocalFile)) {
-        console.log("El archivo temporal ya existe, eliminándolo...");
+        // console.log("El archivo temporal ya existe, eliminándolo...");
         fs.unlinkSync(tempLocalFile); // Elimina si es un archivo
     }
 
     // Descarga el video del bucket a la máquina local
     try {
-        console.log("Iniciando descarga del archivo...");
+        // console.log("Iniciando descarga del archivo...");
         await storage.bucket(bucketName).file(filePath).download({ destination: tempLocalFile });
-        console.log("Archivo descargado correctamente");
+        // console.log("Archivo descargado correctamente");
     } catch (error) {
         console.error("Error al descargar el archivo:", error);
         throw error;
     }
 
-    console.log("Archivo descargado:", tempLocalFile);
+    // console.log("Archivo descargado:", tempLocalFile);
 
     // Asegúrate de que el directorio de frames exista
     if (!fs.existsSync(frameDir)) {
@@ -108,7 +123,6 @@ async function extractFramesFromVideo(filePath, frameRate = 1) {
                         reject(err);
                     } else {
                         // Devuelve las rutas de los frames extraídos
-                        // resolve(files.map(file => path.join(frameDir, file) , tempLocalFile, frameDir ));
                         resolve({ frames: files.map(file => path.join(frameDir, file)), tempLocalFile, frameDir });
                     }
                 });
